fb6067a1b863715941ad9e39b8541dc25d21911a
Umair-Rai <umair.akramcs@gmail.com>
2024-11-30 19:49:53 +0500

    Creating the component

---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up',
+  imports: [],
+  templateUrl: './sign-up.component.html',
+  standalone: true,
+  styleUrl: './sign-up.component.css'
+})
+export class SignUpComponent {
+
+}
