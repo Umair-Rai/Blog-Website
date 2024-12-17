f9e5730d95de779f013208fff0a77cba1b1d480a
NyroXTitan <hayw6121@gmail.com>
2024-12-17 22:28:38 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes'; // Import the routes you defined
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes) // Provide the defined routes
+  ],
+}).catch((err) => console.error(err));
