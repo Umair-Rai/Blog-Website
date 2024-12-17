93ef1892466255d988e891e54baac23df9dea171
NyroXTitan <hayw6121@gmail.com>
2024-12-18 00:09:56 +0500

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,52 +1,27 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from '../footer/footer.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ServicesService } from '../Service/services.service';
-import { NgIf } from '@angular/common';
-
+import { Component,OnInit } from '@angular/core';
+import {ServicesService} from '../Service/services.service';
+import {NgForOf} from '@angular/common';
+import { RouterModule } from '@angular/router'; // Add RouterModule for routing
 @Component({
   selector: 'app-home',
   imports: [
-    FooterComponent,
-    HeaderComponent,
-    FormsModule,
-    ReactiveFormsModule,
-    NgIf,
+    NgForOf,RouterModule,
   ],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
-  standalone: true
+  standalone: true,
+  styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  post = {
-    title: '',
-    author: '',
-    content: '',
-  };
-
-  isSubmitted = false;
-
-  constructor(private service: ServicesService) {}
-
-  onSubmit(form: any): void {
-    if (form.valid) {
-      // Call the service to add the new post
-      this.service.addPost(this.post).subscribe(
-        (response) => {
-          console.log('Post added successfully:', response);
-          this.isSubmitted = true;
-
-          // Hide success message after 3 seconds
-          setTimeout(() => {
-            this.isSubmitted = false;
-            form.resetForm();
-          }, 3000);
-        },
-        (error) => {
-          console.error('Error adding post:', error);
-        }
-      );
-    }
+  constructor(private servicess: ServicesService) {}
+  items!: any;
+  ngOnInit(): void {
+    let item = this.servicess.getdata().subscribe(
+      (data) => {
+        this.items = data;
+      },
+      (error) => {
+        console.log("retrieve failed");
+      });{ }
   }
+
 }
