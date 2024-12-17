07d8815d7a4c72b521814112499edff8c8274ff1
NyroXTitan <hayw6121@gmail.com>
2024-12-17 22:29:56 +0500

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,30 @@
 import { Component } from '@angular/core';
-import {HeaderComponent} from '../header/header.component';
-import {FooterComponent} from '../footer/footer.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {NgIf} from '@angular/common';
+import { RouterModule } from '@angular/router'; // Add RouterModule for routing
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { ServicesService } from '../Service/services.service';
+import {v4 as uuid} from 'uuid';
 
 @Component({
   selector: 'app-home',
-  imports: [
-    FooterComponent,
-    HeaderComponent,
-    FormsModule, ReactiveFormsModule, NgIf,
-  ],
-  templateUrl: './home.component.html',
   standalone: true,
-  styleUrl: './home.component.css'
+  imports: [
+    RouterModule, // Add RouterModule here for routing support
+    HeaderComponent,
+    FooterComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf,
+  ],
+  styleUrl: './home.component.css',
+  templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  constructor(private services: ServicesService) {}
   post = {
+    id: uuid(),
     title: '',
     author:'',
     content: ''
@@ -28,6 +36,13 @@ export class HomeComponent {
     if (form.valid) {
       console.log('Post Data:', this.post);
       this.isSubmitted = true;
+      this.services.senddata(this.post).subscribe(
+        (data) => {
+        console.log("data sent success");
+        },
+      (error) => {
+        console.log("data sent failed");
+      });{ }
 
       // Hide success message after 3 seconds
       setTimeout(() => {
